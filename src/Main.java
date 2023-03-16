0de1d64979ae751de0c0171b06ab83d0fbfb424f
trisha-tomy <trishatomy3.14@gmail.com>
2023-03-16 12:58:19 +0530

    first version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BookDatabase_Library db= new BookDatabase_Library();
+
+        Book c1= new Book("Orion", 73765, "maria", 15);
+        db.create(c1);
+
+        Book c2= db.read("Orion");
+        System.out.println("Book name: "+ c2.getBookName());
+        System.out.println("ISBN: "+ c2.getISBN());
+        System.out.println("Username: "+ c2.getCurrentUser());
+        System.out.println("Days left to renew: "+ c2.getDaysLeftToRenew());
+
+        db.update("Orion", 8237569, "jason", 14);
+
+        System.out.println("Book name: "+ c2.getBookName());
+        System.out.println("ISBN: "+ c2.getISBN());
+        System.out.println("Username: "+ c2.getCurrentUser());
+        System.out.println("Days left to renew: "+ c2.getDaysLeftToRenew());
+
+        db.delete("Orion");
+
+        System.out.println("Book name: "+ c2.getBookName());
+        System.out.println("ISBN: "+ c2.getISBN());
+        System.out.println("Username: "+ c2.getCurrentUser());
+        System.out.println("Days left to renew: "+ c2.getDaysLeftToRenew());
+
     }
 }
